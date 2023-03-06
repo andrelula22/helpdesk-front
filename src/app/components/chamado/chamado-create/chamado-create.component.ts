d1014a5614f4b3a76aef5c5b4593a9da6a7fd622
andrelula22 <andrelula78@gmail.com>
2023-03-06 19:42:18 -0300

    metodo create para chamado implementado

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,6 +1,14 @@
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ChamadoService } from "./../../../services/chamado.service";
+import { TecnicoService } from "./../../../services/tecnico.service";
+import { ClienteService } from "./../../../services/cliente.service";
+import { Tecnico } from "./../../../models/tecnico";
 import { Validators } from "@angular/forms";
 import { FormControl } from "@angular/forms";
 import { Component, OnInit } from "@angular/core";
+import { Cliente } from "src/app/models/cliente";
+import { Chamado } from "src/app/models/chamado";
 
 @Component({
   selector: "app-chamado-create",
@@ -8,20 +16,69 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./chamado-create.component.css"],
 })
 export class ChamadoCreateComponent implements OnInit {
+
+  chamado: Chamado = {
+    prioridade: "",
+    status: "",
+    titulo: "",
+    observacoes: "",
+    tecnico: "",
+    cliente: "",
+    nomeCliente: "",
+    nomeTecnico: "",
+  };
+
+  clientes: Cliente[] = [];
+  tecnicos: Tecnico[] = [];
+
   prioridade: FormControl = new FormControl(null, [Validators.required]);
   status: FormControl = new FormControl(null, [Validators.required]);
   titulo: FormControl = new FormControl(null, [Validators.required]);
-  descricao: FormControl = new FormControl(null, [Validators.required]);
+  observacoes: FormControl = new FormControl(null, [Validators.required]);
   tecnico: FormControl = new FormControl(null, [Validators.required]);
   cliente: FormControl = new FormControl(null, [Validators.required]);
 
-  constructor() {}
+  constructor(
+    private chamadoService: ChamadoService,
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private toast: ToastrService,
+    private route: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.findAllClientes();
+    this.findAllTecnicos();
+  }
+
+  create(): void {
+    this.chamadoService.create(this.chamado).subscribe(resposta => {
+      this.toast.success("Chamado criado com sucesso!", "Novo Chamado");
+      this.route.navigate(['chamados']);
+    }, ex => {
+      this.toast.error(ex.error.error);
+    })
+  }
+
+  findAllClientes(): void {
+    this.clienteService.findAll().subscribe((resposta) => {
+      this.clientes = resposta;
+    });
+  }
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe((resposta) => {
+      this.tecnicos = resposta;
+    });
+  }
 
   validaCampos(): boolean {
-      return this.prioridade.valid && this.status.valid &&
-      this.titulo.valid && this.descricao.valid &&
-      this.tecnico.valid && this.cliente.valid;
+    return (
+      this.prioridade.valid &&
+      this.status.valid &&
+      this.titulo.valid &&
+      this.observacoes.valid &&
+      this.tecnico.valid &&
+      this.cliente.valid
+    );
   }
 }
