18c56b75579b76b4364acdb60ddbc797891754e9
andrelula22 <andrelula78@gmail.com>
2023-03-06 09:49:56 -0300

    criado metodo update para Tecnico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './components/login/login.component';
@@ -12,8 +13,9 @@ const routes: Routes = [
   {
     path:'', component: NavComponent, canActivate:[AuthGuard], children:[
       {path:'home', component:HomeComponent},
+      {path: 'tecnicos', component: TecnicoListComponent},
       {path:'tecnicos/create', component: TecnicoCreateComponent},
-      {path: 'tecnicos', component: TecnicoListComponent}
+      {path:'tecnicos/update/:id', component: TecnicoUpdateComponent},
     ]
   }
 ];
--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl : 'https://helpdesk-backend-production-a4cf.up.railway.app'
+   baseUrl : 'https://helpdesk-backend-production-a4cf.up.railway.app'
+   // baseUrl : 'http://localhost:8080'
 }
