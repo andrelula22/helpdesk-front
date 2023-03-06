01cdcae9c254427bcba1a034bbf5df7b5ec8f575
andrelula22 <andrelula78@gmail.com>
2023-03-06 11:42:34 -0300

    css e html criado para chamado-list

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chamado } from 'src/app/models/chamado';
 
 @Component({
   selector: 'app-chamado-list',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chamado-list.component.css']
 })
 export class ChamadoListComponent implements OnInit {
+  ELEMENT_DATA: Chamado[] = [{
+    
+    id: 1,
+    dataAbertura: '26/02/1998',
+    dataFechamento:'26/02/1998',
+    prioridade: 'ALTA',
+    status:'ANDAMENTO',
+    titulo:'chamado 1',
+    descricao:'Teste de chamado',
+    tecnico:1,
+    cliente:3,
+    nomeCliente:'Andre lindo',
+    nomeTecnico:'Albert Einstein'
+  }
+  ];
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'status', 'prioridade', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor() {}
 
   ngOnInit(): void {
+    
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  /*
+  findAll(){
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta
+      this.dataSource = new MatTableDataSource<Chamado>(resposta);
+      this.dataSource.paginator = this.paginator;
+    })
+  }*/
 
 }
